d95cd96ea8511090920599f1e022cad6c7e320c0
therealozp <anhkhang.le0910@gmail.com>
2023-07-03 00:25:45 -0400

    this will work finally (i commented out quotefiller)

--- pages/testpage.js
+++ pages/testpage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import QuoteFiller from '../src/components/fillers/QuoteFiller';
+// import QuoteFiller from '../src/components/fillers/QuoteFiller';
 import ContactBlock from '../src/components/blocks/ContactBlock';
 const WrapperNoSSR = dynamic(() => import('../src/components/extra/Wrapper'), {
 	ssr: false,
